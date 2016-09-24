edca51b5605215c8c0647504d2c873b935a65691
Gosilama <osilamagodspower@gmail.com>
2016-09-24 12:29:33 +0100

    First Commit

---
+++ Paperless Hospital/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Paperless_Hospital.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Paperless Hospital/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Paperless_Hospital.Startup))]
+namespace Paperless_Hospital
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
