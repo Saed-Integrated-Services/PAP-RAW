9f56ffc3590d4324cd1e996f61a08886f89dc2f6
Gosilama <osilamagodspower@gmail.com>
2016-10-03 19:34:39 +0100

    database connection

--- Paperless Hospital/Account/Register.aspx.cs
+++ Paperless Hospital/Account/Register.aspx.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Owin;
 using Paperless_Hospital.Models;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace Paperless_Hospital.Account
 {
@@ -13,32 +15,51 @@ namespace Paperless_Hospital.Account
     {
         protected void CreateUser_Click(object sender, EventArgs e)
         {
-            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
-            var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
-            try
+            if (!this.IsPostBack)
             {
-                IdentityResult result = manager.Create(user, Password.Text);
-                if (result.Succeeded)
-                {
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    //string code = manager.GenerateEmailConfirmationToken(user.Id);
-                    //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
-                    //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
+                String constr = ConfigurationManager.ConnectionStrings["regConn"].ConnectionString;
 
-                    signInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
-                    IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-                }
-                else
+                using (MySqlConnection conn = new MySqlConnection(constr))
                 {
-                    ErrorMessage.Text = result.Errors.FirstOrDefault();
+                    conn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand("INSERT INTO login VALUES ('" + Email.Text + "','" + Password.Text + "')"))
+                    {
+                        cmd.ExecuteNonQuery();
+                        //using (MySqlDataAdapter sda = new MySqlDataAdapter())
+                        //{
+                        //    cmd.Connection = conn;
+                        //    sda.SelectCommand = cmd;
+                        //}
+                    }
                 }
             }
-            catch (Exception)
-            {
-                ErrorMessage.Text = "Unable to connect to database. Contact Administrator";
-            }
-            
+
+            //var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            //var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
+            //var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
+            //try
+            //{
+            //    IdentityResult result = manager.Create(user, Password.Text);
+            //    if (result.Succeeded)
+            //    {
+            //        // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+            //        //string code = manager.GenerateEmailConfirmationToken(user.Id);
+            //        //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
+            //        //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
+
+            //        signInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
+            //        IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+            //    }
+            //    else
+            //    {
+            //        ErrorMessage.Text = result.Errors.FirstOrDefault();
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    ErrorMessage.Text = "Unable to connect to database. Contact Administrator";
+            //}
+
         }
     }
 }
